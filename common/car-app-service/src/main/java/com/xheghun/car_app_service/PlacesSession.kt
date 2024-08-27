ab0ccccd156c15d1f0cb3c9d70a5c3069e274f05
Xheghun <ysenruf@gmail.com>
2024-08-27 23:11:36 +0100

    implement main screen

--- common/car-app-service/src/main/java/com/xheghun/car_app_service/PlacesSession.kt
+++ common/car-app-service/src/main/java/com/xheghun/car_app_service/PlacesSession.kt
@@ -3,9 +3,10 @@ package com.xheghun.car_app_service
 import android.content.Intent
 import androidx.car.app.Screen
 import androidx.car.app.Session
+import com.xheghun.car_app_service.screen.MainScreen
 
 class PlacesSession : Session() {
     override fun onCreateScreen(intent: Intent): Screen {
-        TODO("Not yet implemented")
+        return MainScreen(carContext)
     }
 }
