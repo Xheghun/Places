e7e85684235eab2d97cc1cc2824722e0526986a7
Xheghun <ysenruf@gmail.com>
2024-08-27 23:06:27 +0100

    Initial commit

---
+++ common/car-app-service/src/main/java/com/xheghun/car_app_service/PlacesSession.kt
@@ -0,0 +1,11 @@
+package com.xheghun.car_app_service
+
+import android.content.Intent
+import androidx.car.app.Screen
+import androidx.car.app.Session
+
+class PlacesSession : Session() {
+    override fun onCreateScreen(intent: Intent): Screen {
+        TODO("Not yet implemented")
+    }
+}
