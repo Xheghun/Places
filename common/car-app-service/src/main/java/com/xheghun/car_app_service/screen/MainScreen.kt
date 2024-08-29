7fc49b51c1cceff3ac41757c1cbb389719a1700d
Xheghun <ysenruf@gmail.com>
2024-08-29 11:42:34 +0100

    add places map

--- common/car-app-service/src/main/java/com/xheghun/car_app_service/screen/MainScreen.kt
+++ common/car-app-service/src/main/java/com/xheghun/car_app_service/screen/MainScreen.kt
@@ -1,25 +1,60 @@
 package com.xheghun.car_app_service.screen
 
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannedString
 import androidx.car.app.CarContext
 import androidx.car.app.Screen
 import androidx.car.app.model.Action
+import androidx.car.app.model.CarLocation
+import androidx.car.app.model.Distance
+import androidx.car.app.model.DistanceSpan
+import androidx.car.app.model.ItemList
+import androidx.car.app.model.Metadata
 import androidx.car.app.model.Pane
 import androidx.car.app.model.PaneTemplate
+import androidx.car.app.model.Place
+import androidx.car.app.model.PlaceListMapTemplate
+import androidx.car.app.model.PlaceMarker
 import androidx.car.app.model.Row
 import androidx.car.app.model.Template
+import com.example.places.data.PlacesRepository
 
 class MainScreen(carContext: CarContext) : Screen(carContext) {
     override fun onGetTemplate(): Template {
-        val row = Row.Builder()
-            .setTitle("Main Screen")
-            .build()
 
-        val pane = Pane.Builder()
-            .addRow(row)
-            .build()
+        val placeRepo = PlacesRepository()
+        val itemListBuilder = ItemList.Builder()
+            .setNoItemsMessage("No Places to show")
 
-        return PaneTemplate.Builder(pane)
-            .setHeaderAction(Action.APP_ICON)
+        placeRepo.getPlaces()
+            .forEach {
+                itemListBuilder.addItem(
+                    Row.Builder()
+                        .setTitle(it.name)
+                        .addText(SpannableString(" ").apply {
+                            setSpan(
+                                DistanceSpan.create(
+                                    Distance.create(Math.random() * 100, Distance.UNIT_KILOMETERS)
+                                ), 0, 1, Spannable.SPAN_INCLUSIVE_INCLUSIVE
+                            )
+                        })
+                        .setOnClickListener {  }
+                        .setMetadata(
+                            Metadata.Builder(  )
+                                .setPlace(Place.Builder(
+                                    CarLocation.create(it.latitude, it.longitude))
+                                    .setMarker(PlaceMarker.Builder().build())
+                                    .build())
+                                .build()
+                        )
+                        .build()
+                )
+            }
+
+        return PlaceListMapTemplate.Builder()
+            .setTitle("Places")
+            .setItemList(itemListBuilder.build())
             .build()
     }
 }
--- places/build.gradle.kts
+++ places/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.xheghun.places"
-        minSdk = 28
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
